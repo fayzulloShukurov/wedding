8634d710d432481b0c7e66dc14da10795a5fc7b6
fayzullo <105875268+fayzulloShukurov@users.noreply.github.com>
2024-03-31 16:19:40 +0700

    Finished

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open")
+}
+
+var countDownDate = new Date("July 27, 2024 00:00:00 ").getTime();
+var x = setInterval(function(){
+var now = new Date().getTime();
+var distance = countDownDate - now;
+
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+
+}, 1000);
